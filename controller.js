22d68ffc39fe5621222f6467a91117491a79637f
Jesse Boucher <jesseboucher8@gmail.com>
2017-06-06 19:41:14 -0600

    completed project with api

--- controller.js
+++ controller.js
@@ -1,10 +1,23 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	mainService.getUsers().then(function(response) {
+  		$scope.users = response.data.data;
+  	});
   }
-
-
   $scope.getUsers();
+
+
+  $scope.toggleFavorite = function(id) {
+  	for(var i = 0; i < $scope.users.length; i++) {
+        if($scope.users[i].id === id) {
+            $scope.users[i].isFavorite = !$scope.users[i].isFavorite;
+            }
+        }
+  	};
+
+
+
 
 });
